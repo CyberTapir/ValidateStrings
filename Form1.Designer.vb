22be92fc64fe0a8934240635dee8de9e0d371129
cybertapir <callumtb@CXB-PC>
2023-10-30 09:36:07 +1100

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,72 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TextBox1 = New TextBox()
+        ListBox1 = New ListBox()
+        Button1 = New Button()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(292, 186)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(100, 23)
+        TextBox1.TabIndex = 0
+        ' 
+        ' ListBox1
+        ' 
+        ListBox1.FormattingEnabled = True
+        ListBox1.ItemHeight = 15
+        ListBox1.Location = New Point(544, 135)
+        ListBox1.Name = "ListBox1"
+        ListBox1.Size = New Size(120, 94)
+        ListBox1.TabIndex = 1
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(317, 249)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 2
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Button1)
+        Controls.Add(ListBox1)
+        Controls.Add(TextBox1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button1 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,34 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim validateText As String
+        validateText = TextBox1.Text
+        ListBox1.Items.Add(IsValidCode(validateText))
+    End Sub
+    Function IsValidCode(code As String) As Boolean
+        If code.Length <> 5 Then
+            Return False
+        End If
+
+        If Not Char.IsLetter(code(0)) OrElse Not Char.IsLetter(code(4)) Then
+            Return False
+        End If
+
+        If Not Char.IsDigit(code(1)) OrElse Not Char.IsDigit(code(2)) OrElse Not Char.IsDigit(code(3)) Then
+            Return False
+        End If
+
+        Dim firstLetter As Char = code(0)
+        Dim lastLetter As Char = code(4)
+
+        If firstLetter < "A"c OrElse firstLetter > "P"c OrElse firstLetter = "I"c OrElse firstLetter = "O"c Then
+            Return False
+        End If
+
+        If lastLetter < "A"c OrElse lastLetter > "P"c OrElse lastLetter = "I"c OrElse lastLetter = "O"c Then
+            Return False
+        End If
+
+        Return True
+    End Function
+
+End Class
